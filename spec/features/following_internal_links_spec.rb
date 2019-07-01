280e75189a40208dcee987e6ab98523986f74a7c
Duncan Brown <duncanjbrown@gmail.com>
2019-07-01 15:40:11 +0100

    Add Usage scenarios

--- spec/features/following_internal_links_spec.rb
+++ spec/features/following_internal_links_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'following resource links', type: :feature do
+describe 'following internal links', type: :feature do
   scenario 'clicking the "Contact details" link in the "Application" attributes list' do
     visit '/'
 
@@ -10,5 +10,15 @@ describe 'following resource links', type: :feature do
     end
 
     expect(page).to have_content 'The candidate’s phone number'
+  end
+
+  scenario 'clicking the "Usage scenarios" link on the front page' do
+    visit '/'
+
+    within '#content' do
+      click_on 'usage scenarios'
+    end
+
+    expect(page).to have_content 'A successful application'
   end
 end
