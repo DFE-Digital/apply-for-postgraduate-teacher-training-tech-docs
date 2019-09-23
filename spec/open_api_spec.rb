217290b705b1791ad47289ec61f00b82e3605ca9
Tijmen Brommet <tijmen@gmail.com>
2019-09-23 12:11:10 +0100

    Make all fields required
    This tells the consumers of the API that all of the fields will always
    be returned. Some of the fields will be allowed to be nil though, but
    that's for a follow up commit.

--- spec/open_api_spec.rb
+++ spec/open_api_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe "OpenAPI spec" do
   it "is a valid OpenAPI spec" do
     expect(document).to be_valid, document.errors.to_a.inspect
   end
+
+  document.components.schemas.each do |schema_name, schema|
+    it "requires all of the keys to be present for schema #{schema_name}" do
+      required_properties = schema.required.to_a
+      actual_properties = schema.properties.map { |property_name, _| property_name }
+
+      expect(required_properties).to match_array(actual_properties)
+    end
+  end
 end
